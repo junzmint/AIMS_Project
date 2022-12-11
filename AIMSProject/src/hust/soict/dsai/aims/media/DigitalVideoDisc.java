a02c9bd5b2c49861c20e5cc4c2ee44e177d657f7
Bui Minh Dung <85019719+junzmint@users.noreply.github.com>
2022-12-11 17:16:15 +0700

    update-class-diagram

--- AIMSProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AIMSProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,7 +1,6 @@
 package hust.soict.dsai.aims.media;
 
 public class DigitalVideoDisc extends Disc implements Playable {
-	private static int nbDigitalVideoDiscs = 0;
 	
 	public DigitalVideoDisc() {
 		super();
